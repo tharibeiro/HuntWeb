71b8bb6f8244980d6b64719ea25a2bb272c0c214
Thaina Costa <thainaribeiro.lima@outlook.com.br>
2019-06-04 18:25:34 -0300

    Armazenando os produtos em um state

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,18 +2,25 @@ import React, {Component} from 'react';
 import api from "../../services/api";
 
 export default class Main extends Component {
+    state = {
+        products: []
+    }
     componentDidMount(){
        this.loadProducts();
     }
     loadProducts = async () =>{
         const response = await api.get("/products");
 
-        console.log(response.data.docs);
+        this.setState({ products: response.data.docs })
     }
 
-
-
     render(){
-        return <h1>Hello Rocketseat</h1>
+        return (
+            <div className="product-list">
+                {this.state.products.map(product =>(
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        );
     }
 }
